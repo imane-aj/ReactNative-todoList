862655381b632769a3a2422bd343c076b2ce449b
imane-aj <ajroudi.im@gmail.com>
2023-02-13 13:19:01 +0100

    updates

--- TodoList/Components/Viewer.js
+++ TodoList/Components/Viewer.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Button, Pressable, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export const Viewer = () => {
+    const [show, setShow] = useState(false)
+    console.log(show)
   return (
     <View style={styles.hero}>
-        <Text style={styles.title}>
+        <Text style={styles.title1}>
              Todo <Text style={styles.span}>List</Text> 
         </Text>
-        <Button title="+" style={styles.button}/>
+        {show ? 
+         <Pressable style={styles.button} onPress={()=>{setShow(true)}}>
+            <Text style={styles.text}>+</Text>
+        </Pressable>
+        :
+        <Pressable style={styles.button} onPress={()=>{setShow(false)}}>
+            <Text style={styles.text}>-</Text>
+        </Pressable>
+        }
+       
     </View>
   );
 }
 const styles = StyleSheet.create({
     hero:{
         marginTop: 50
-        
     },
-    title: {
+    title1: {
         fontSize: 30,
         fontWeight: 'bold',
     },
@@ -24,7 +34,16 @@ const styles = StyleSheet.create({
         color: 'purple',
         fontWeight: 'normal',
     },
-    button:{
-        backgroundColor: "white"
-    }
+    button: {
+        alignItems: "center",
+        marginTop: 10
+    },
+    text: {
+        width: 40,
+        borderWidth: 1,
+        borderColor: "purple",
+        borderRadius: 3,
+        textAlign: 'center',
+        paddingVertical: 10,
+    },
   });
